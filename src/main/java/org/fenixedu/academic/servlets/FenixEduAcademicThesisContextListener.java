c6f2907881c8d4e8aa87d269b8cf83dcc6c2583c
Miguel Pasadinhas <miguel.pasadinhas@tecnico.ulisboa.pt>
2017-01-18 16:53:59 +0000

    Associate thesis manager group to Degree upon creation

---
+++ src/main/java/org/fenixedu/academic/servlets/FenixEduAcademicThesisContextListener.java
@@ -0,0 +1,26 @@
+package org.fenixedu.academic.servlets;
+
+import org.fenixedu.academic.domain.Degree;
+import org.fenixedu.academic.domain.DegreeHelper;
+import org.fenixedu.academic.domain.accessControl.PersistentCoordinatorGroup;
+import org.fenixedu.bennu.signals.DomainObjectEvent;
+import org.fenixedu.bennu.signals.Signal;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class FenixEduAcademicThesisContextListener implements ServletContextListener {
+    @Override public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Signal.register(Degree.CREATED_SIGNAL, (DomainObjectEvent<Degree> event) -> {
+            Degree degree = event.getInstance();
+            PersistentCoordinatorGroup pcg = PersistentCoordinatorGroup.getInstance(degree.getDegreeType(), degree);
+            DegreeHelper.setCanManageThesis(degree, pcg.toGroup());
+        });
+    }
+
+    @Override public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
