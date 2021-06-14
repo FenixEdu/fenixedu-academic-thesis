FenixEdu/fenixedu-academic-thesis

commit 3950ac5a0fd167bdb5cabdc265462ed01e3af61f

Fix candidacies details not showing correctly

src/main/webapp/WEB-INF/studentCandidacies/list.jsp
@@ -93,14 +93,6 @@
 
 			}
 		} );
-		$('.existingCandidaciesTable').DataTable().on( 'draw', function () {
-			$(".detailsButton").on("click", function(evt){
-				var e = $(evt.target);
-				var tid = e.data('thesis');
-
-				$('#view').modal('show');
-			});
-		} );
 		// $('.dataTables_filter').remove();
 	});
 </script>
@@ -309,14 +301,8 @@ $(document).ready(function() {
 </style>
 
 <script type="text/javascript">
-jQuery(document).ready(function(){
-	jQuery('.detailsButton').on('click', function(event) {
-		$("#details" + $(this).data("thesis")).toggle('show');
-	});
-});
-
 $(function(){
-	$(".detailsButton").on("click", function(evt){
+	$(".existingCandidaciesTable tbody").on("click", ".detailsButton", function(evt){
 		var e = $(evt.target);
 		var tid = e.data('thesis');
 

commit c462db1fc965efe36420135c1e4253bcce8cfffb

Fix details button is my candidacies table not working
Bug introduced in 3950ac5a0fd167bdb5cabdc265462ed01e3af61f (PR #97)

src/main/webapp/WEB-INF/studentCandidacies/list.jsp
@@ -311,7 +311,7 @@ function escapeHtml(unsafe) {
 }
 
 $(function(){
-	$(".existingCandidaciesTable tbody").on("click", ".detailsButton", function(evt){
+	$("#candidaciesTable tbody").on("click", ".detailsButton", function(evt){
 		var e = $(evt.target);
 		var tid = e.data('thesis');
 